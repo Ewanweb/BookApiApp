e8e57d6f3707e1970faca58465feb1911ea5648b
mahan mahmodi <mahan01ms@gmail.com>
2025-01-02 11:12:28 +0330

    Add Order Aggregate

---
+++ Shop/Shop.Domain/OrderAgg/ValueObjects/OrderDiscount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Domain;
+
+namespace Shop.Domain.OrderAgg.ValueObjects
+{
+    public class OrderDiscount : ValueObject
+    {
+        public string DiscountTitle { get; private set; }
+        public int DiscountAmount { get; private set; }
+    }
+}
