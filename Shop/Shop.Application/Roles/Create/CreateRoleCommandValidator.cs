260457b5316228a8e64e758c414df7118391b6ed
ماهان محمودی <MAHAN001MS@GMAIL.COM>
2025-01-10 15:50:27 +0330

    Role Compelete

---
+++ Shop/Shop.Application/Roles/Create/CreateRoleCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using Common.Application.Validation;
+using FluentValidation;
+
+namespace Shop.Application.Roles.Create
+{
+    public class EditRoleCommandValidator : AbstractValidator<EditRoleCommand>
+    {
+        public EditRoleCommandValidator()
+        {
+            RuleFor(r => r.Title)
+                .NotEmpty().WithMessage(ValidationMessages.required("عنوان"));
+
+        }
+    }
+}
---
+++ Shop/Shop.Application/Roles/Edit/EditRoleCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Common.Application;
+using Shop.Domain.RoleAgg;
+using Shop.Domain.RoleAgg.Repository;
+
+namespace Shop.Application.Roles.Edit
+{
+    public class EditRoleCommandHandler : IBaseCommandHandler<EditRoleCommand>
+    {
+        private readonly IRoleRepository _roleRepository;
+
+        public EditRoleCommandHandler(IRoleRepository roleRepository)
+        {
+            _roleRepository = roleRepository;
+        }
+
+        public async Task<OperationResult> Handle(EditRoleCommand request, CancellationToken cancellationToken)
+        {
+            var role = await _roleRepository.GetTracking(request.Id);
+            if (role == null)
+                return OperationResult.NotFound();
+
+            role.Edit(request.Title);
+
+            var permission = new List<RolePermission>();
+
+            request.Permissions.ForEach(f =>
+            {
+                permission.Add(new RolePermission(f));
+            });
+            role.SetPermission(permission);
+
+            await _roleRepository.Save();
+            return OperationResult.Success();
+        }
+    }
+}
