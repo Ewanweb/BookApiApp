e631064436dc549ee66dc55f1c6a3e166c380432
ماهان محمودی <MAHAN001MS@GMAIL.COM>
2025-01-18 17:02:49 +0330

    Complete Application

---
+++ Shop/Shop.Application/SiteEntities/Banners/Edit/EditBannerCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using Common.Application;
+using Common.Application.FileUtil.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Shop.Application.Utilities;
+using Shop.Domain.SiteEntities;
+using Shop.Domain.SiteEntities.Repository;
+
+namespace Shop.Application.SiteEntities.Banners.Edit
+{
+    public class EditBannerCommandHandler : IBaseCommandHandler<EditBannerCommand>
+    {
+        private readonly IBannerRepository _repository;
+        private readonly IFileService _fileService;
+
+        public EditBannerCommandHandler(IBannerRepository repository, IFileService fileService)
+        {
+            _repository = repository;
+            _fileService = fileService;
+        }
+        public async Task<OperationResult> Handle(EditBannerCommand request, CancellationToken cancellationToken)
+        {
+            var banner = await _repository.GetTracking(request.Id);
+
+            if (banner == null)
+                return OperationResult.NotFound();
+
+            var imageName = banner.ImageName;
+
+            var oldImage = banner.ImageName;
+
+            if (request.ImageFile != null)
+                imageName = await _fileService.SaveFileAndGenerateName(request.ImageFile, Directories.Banner);
+
+            banner.Edit(request.Link, imageName, request.Position);
+            await _repository.Save();
+
+            DeleteImage(request.ImageFile, oldImage);
+
+            return OperationResult.Success();
+        }
+
+        private void DeleteImage(IFormFile? imageFile, string oldImage)
+        {
+            if (imageFile != null)
+                _fileService.DeleteFile(Directories.Slider, oldImage);
+        }
+    }
+}
---
+++ Shop/Shop.Application/SiteEntities/Sliders/Create/CreateSliderCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Common.Application;
+using Common.Application.FileUtil.Interfaces;
+using Shop.Application.Utilities;
+using Shop.Domain.SiteEntities;
+using Shop.Domain.SiteEntities.Repository;
+
+namespace Shop.Application.SiteEntities.Sliders.Create
+{
+    public class CreateSliderCommandHandler : IBaseCommandHandler<CreateSliderCommand>
+    {
+        private readonly ISliderRepository _repository;
+        private readonly IFileService _fileService;
+
+        public CreateSliderCommandHandler(ISliderRepository repository, IFileService fileService)
+        {
+            _repository = repository;
+            _fileService = fileService;
+        }
+
+        public async Task<OperationResult> Handle(CreateSliderCommand request, CancellationToken cancellationToken)
+        {
+            var imageName = await _fileService.SaveFileAndGenerateName(request.ImageFile, Directories.Slider);
+
+            var slider = new Slider(request.Title, request.Link, imageName);
+
+            _repository.Add(slider);
+            await _repository.Save();
+            return OperationResult.Success();
+        }
+    }
+}
