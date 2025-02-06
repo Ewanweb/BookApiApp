ba97fc38d9141731a705ea61978c62999e9a9ae3
ماهان محمودی <MAHAN001MS@GMAIL.COM>
2025-02-06 17:14:15 +0330

    update

---
+++ Shop/Shop.Query/Orders/DTOs/OrderItenDto.cs
@@ -0,0 +1,12 @@
+﻿using Common.Query;
+
+namespace Shop.Query.Orders.DTOs;
+
+public class OrderItenDto : BaseDto
+{
+    public long OrderId { get; internal set; }
+    public long InventoryId { get; private set; }
+    public int Count { get; private set; }
+    public int Price { get; private set; }
+    public int TotalPrice => Price * Count;
+}
